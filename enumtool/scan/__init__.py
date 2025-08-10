5823ea0adaa888c73f36c6d97c6e452baf357df3
lezzel2015 <mtorrese75@gmail.com>
2025-08-10 22:53:41 +0200

    Delete enumtool directory

--- enumtool/scan/__init__.py
+++
@@ -1,5 +0,0 @@
-# scan package
-
-from .tcp_connect import tcp_connect
-from .syn_scan import syn_scan
-from .ack_scan import ack_scan
--- enumtool/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-# utils package
-from .network import expand_targets
-from .ports import parse_ports, PortParseError
